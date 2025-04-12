c37e473d0af4e0963722253a6a1626f227c6a0b4
Maximilian Hutter <greatmiralis@gmail.com>
2025-04-12 14:25:28 +0200

    improved the filter and added an average and split dataset into eval and training set

--- values_filter_table.py
+++ values_filter_table.py
@@ -1,9 +1,11 @@
 max_filtertable = {
     "win_prob" : 1.00,
     "training_time" : 7096000.0, # is substantially bigger than the the next smallest one. need more testing if outlier or not
+    "speed" : 6000
 }
 
 low_filtertable = {
     "win_prob" : 0.00,
     "training_time" : 0.0,
+    "speed" : 600
 }

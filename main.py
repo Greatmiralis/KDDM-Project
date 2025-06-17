3540bc385c5ec483ff7ef1522e5ed7863b47c7d0
Eva Haring <eva.haring@student.tugraz.at>
2025-06-17 14:41:38 +0200

    comments for main

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import numpy as np
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.metrics import r2_score, mean_squared_error
 from sklearn.compose import TransformedTargetRegressor
+
+
 model = Pipeline( steps=[
     ("preprocessor", preprocessor), # preprocessing
     #("corr_filter", CorrelationFilter(threshold = 0.9)), # filter all columns with correlation over threshold
@@ -17,7 +19,6 @@ model = Pipeline( steps=[
 )
 
 def outputTransformedDFs(x, y, y_transformed):
-    
     x_transformed = preprocessor.fit_transform(x)
     cat_pipe = preprocessor.named_transformers_["cat"]
     onehot_encoder = cat_pipe.named_steps["encoder"]
@@ -40,7 +41,6 @@ def outputTransformedDFs(x, y, y_transformed):
     df_combined.to_csv("transformed_data", index=False)
     
 def compareArrays(arr1, arr2):
-    
     arr2 = arr2.to_numpy().flatten()
     mask = arr2 <= 1
     
@@ -59,18 +59,20 @@ def compareArrays(arr1, arr2):
     plt.show()
 
 if __name__ == "__main__":
+    # load and filter data
     df = pd.read_csv("Data-20250331/data.csv")
     df_filtered = df[df["win_prob"] <= 1].copy()
-    
+
+    # train test split
     x = df_filtered.drop("win_prob", axis=1)
     y = df_filtered[["win_prob"]]
-        
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=33)
 
+    # target transformation
     y_transformed = target_transform.fit_transform(y_train)
     outputTransformedDFs(X_train, y_train, y_transformed)
     
-
+    # train model
     model.fit(X_train, y_transformed.ravel())
     pred = model.predict(X_test)
     bias = np.mean(y_test.to_numpy().flatten() - pred)
@@ -79,7 +81,8 @@ if __name__ == "__main__":
     bias = np.mean(y_test.to_numpy().flatten() - pred)
     print("bias post: ", bias)
     compareArrays(pred, y_test)
-    
+
+    # evaluation
     r2 = r2_score(y_test, pred)
     print("R²:", r2)
     
@@ -92,17 +95,20 @@ if __name__ == "__main__":
     df_combined["residuals"] = df_combined["Target values"] - df_combined["Prediction"]
     df_combined.to_csv("Target_and_predictions", index=False)
     
-    # Task 2
-    
+    ######### Task 2 Predict win probabilities #########
+    # load data
     x = pd.read_csv("Data-20250331/Task2-superheroes-villains-edited.csv")
     name = x["name"]
+
+    # predict win probabilities
     pred = model.predict(x)
     print("Prediction: ", pred)
     pred_df = pd.DataFrame(pred)
     pred_df.columns = ["pred win_prob"]
     df_combined = pd.concat([x.reset_index(drop=True), pred_df.reset_index(drop=True)], axis=1)
     df_combined.to_csv("Task2 win probs", index=False)
-    
+
+    # get feature names
     preprocessor = model.named_steps['preprocessor']
     cat_pipe = preprocessor.named_transformers_["cat"]
     onehot_encoder = cat_pipe.named_steps["encoder"]
@@ -116,7 +122,10 @@ if __name__ == "__main__":
     num_encode = num_pipe.named_steps["poly"]
     num_feat_names = num_encode.get_feature_names_out(numerical_features)
 
+    # combined feature names
     all_feature_names = list(num_feat_names) + list(onehot_feature_names) + list(role_feat_names)
+
+    # loop over all characters and explain predictions
     for i in range(x.shape[0]):
         row = x.iloc[[i]]
         
@@ -137,9 +146,12 @@ if __name__ == "__main__":
         explanation.to_csv("Task2 feature contribution" + row["name"].values[0], index=False)
     
     
-    # Task3
-    
+    ########## Task3 Villain Analysis #########
+
+    # load data
     x = pd.read_csv("Data-20250331/Task3_villain.csv")
+
+    # prediction
     pred = model.predict(x.drop(" win_prob", axis=1))
     print("Prediction: ", pred)
     pred_df = pd.DataFrame(pred)
@@ -149,6 +161,7 @@ if __name__ == "__main__":
     
     row = x.iloc[[0]]
 
+    # transform data and retreive coefficients and intercept
     X_transformed = preprocessor.transform(row)
     coefficients = model.named_steps['ridge'].coef_ 
     intercept = model.named_steps['ridge'].intercept_
